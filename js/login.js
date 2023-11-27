4a22bbffd93d760ff1073a488e101785c3d84b55
Jose <138242710+perazzajose@users.noreply.github.com>
2023-11-27 10:19:03 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -8,39 +8,32 @@ document.addEventListener("DOMContentLoaded", () => {
   const currentUser = localStorage.getItem("currentUser");
 
   if (currentUser) {
-    //aca va el primer href
+    window.location.href = "index.html";
+    // Verificar si el usuario ya tiene una foto de perfil en el localStorage
     const user = database.users.find((user) => user.username === currentUser);
     if (!user.profilePicture) {
-      user.profilePicture = "default-profile.jpg"; 
+      // Si no tiene una foto de perfil, establece una foto predeterminada
+      user.profilePicture = "default-profile.jpg"; // Nombre de archivo de la foto predeterminada
       saveDatabase(databaseKey, database);
     }
   }
 
-  loginForm.addEventListener("submit", async (e) => {
+  loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const identifier = document.getElementById("loginIdentifier").value;
     const password = document.getElementById("loginPassword").value;
 
-    try {
-      const response = await fetch('http://localhost:3001/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username: identifier, password })
-      });
-
-      if (response.ok) {
-        const { token } = await response.json();
-        localStorage.setItem("token", token);
-        window.location.href = "index.html"
-      } else {
-        messageDiv.innerHTML = "Credenciales inválidas.";
-      }
-    } catch (error) {
-      console.error('Error durante el inicio de sesión:', error);
-      messageDiv.innerHTML = "Error durante el inicio de sesión.";
+    // Verificar las credenciales
+    const user = database.users.find(
+      (user) =>
+        (user.username === identifier || user.email === identifier) &&
+        user.password === password
+    );
+    if (user) {
+      localStorage.setItem("currentUser", user.username);
+      window.location.href = "index.html";
+    } else {
+      messageDiv.innerHTML = "Credenciales inválidas.";
     }
   });
-  
 });
